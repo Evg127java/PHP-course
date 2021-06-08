e94aa921203577cf00ed112153fde91a220f28f1
User <kristall@ua.fm>
2021-06-09 00:19:01 +0300

    level2 api v1. PHPdoc-blocks to functions added

--- level2/back/api/v1/functions.php
+++ level2/back/api/v1/functions.php
@@ -3,16 +3,31 @@
 require_once 'headers.php';
 require_once 'constants.php';
 
+/**
+ * Gets decoded items from the json file
+ *
+ * @return mixed
+ */
 function getItemsFromFile()
 {
     return json_decode(file_get_contents(__DIR__ . '/' . JSON_DATA_FILE), true);
 }
 
+/**
+ * Gets data from input stream
+ *
+ * @return mixed
+ */
 function getInputData()
 {
     return json_decode(file_get_contents(PHP_INPUT), true);
 }
 
+/**
+ * Gets the last assigned ID of an item
+ *
+ * @return int ID value
+ */
 function getLastID(): int
 {
     $lastID = file_get_contents(__DIR__ . '/' . LAST_ID_FILE);
