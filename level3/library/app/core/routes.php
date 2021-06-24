4f64bc0a3bdaba6ee6bffc856dcf185f1b261730
User <kristall@ua.fm>
2021-06-24 18:47:31 +0300

    New routes added

--- level3/library/app/core/routes.php
+++ level3/library/app/core/routes.php
@@ -1,8 +1,12 @@
 <?php
 
 use app\controllers\BooksController;
+use app\controllers\ServiceController;
 
 return [
     '~^/(\d*)$~' => [BooksController::class, 'showAll'],
     '~^/book/(\d+)$~' => [BooksController::class, 'showOne'],
+    '~^/migration$~' => [ServiceController::class, 'migration'],
+    '~^/books/search/(.*)$~' => [BooksController::class, 'search'],
+    '~^/admin/(\d*)$~' => [AdminController::class, 'main'],
 ];
